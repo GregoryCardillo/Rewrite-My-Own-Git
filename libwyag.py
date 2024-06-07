64d96efd5e3bf31d9d496f86e4b48148ac2acfdc
Gregory Cardillo <gregoryincmail@gmail.com>
2024-06-07 14:03:26 +0200

    Add cat-file command and hash-object command

--- libwyag.py
+++ libwyag.py
@@ -184,8 +184,8 @@ class GitObject (object):
     def serialize(self, repo):
         """This function MUST be implemented by subclasses.
 
-It must read the object's contents from self.data, a byte string, and do
-whatever it takes to convert it into a meaningful representation.  What exactly that means depend on each subclass."""
+    It must read the object's contents from self.data, a byte string, and do
+    whatever it takes to convert it into a meaningful representation.  What exactly that means depend on each subclass."""
         raise Exception("Unimplemented!")
 
     def deserialize(self, data):
@@ -227,3 +227,33 @@ def object_read(repo, sha):
 
         # Call constructor and return object
         return c(raw[y+1:])
+    
+
+def object_write(obj, repo=None):
+    # Serialize object data
+    data = obj.serialize()
+    # Add header
+    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+    # Compute hash
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+        # Compute path
+        path=repo_file(repo, "objects", sha[0:2], sha[2:], mkdir=True)
+
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                # Compress and write
+                f.write(zlib.compress(result))
+    return sha
+
+
+class GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
